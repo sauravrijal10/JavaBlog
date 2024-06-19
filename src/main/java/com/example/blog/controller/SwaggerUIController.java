4b8a906acd4dba6919b87df0c77c1345b757708a
sauravrijal10 <sauravrijal1011@gmail.com>
2024-06-19 21:16:05 +0545

    Swagger implemented

---
+++ src/main/java/com/example/blog/controller/SwaggerUIController.java
@@ -0,0 +1,13 @@
+// package com.example.blog.controller;
+
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RequestMethod;
+// import org.springframework.web.bind.annotation.RestController;
+
+// @RestController
+// public class SwaggerUIController {
+//     @RequestMapping(value = "/swagger-ui", method = RequestMethod.GET)
+//     public String redirectToSwagger() {
+//         return "redirect:/swagger-ui/index.html";
+//     }
+// }
